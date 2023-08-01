0f51bb373abe41d914289078d08463854e18d8c8
Srimanta Pal <srimantapal205@hotmail.com>
2023-08-01 23:43:04 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ctr">
+    <Question />
+    <Result/>
+    <button type="button" class="reset-btn">Reset</button>
+  </div>
+</template>
+
+<script>
+import Question from "./components/Question.vue";
+import Result from "./components/Result.vue";
+import quizData from "./data/data.js";
+
+export default {
+  name: "App",
+  components: {
+    Question,
+    Result,
+  },
+  data() {
+    return {
+      questionAnswerd:0,
+      question: quizData,
+    };
+  },
+};
+</script>
+
+<style lang="scss"></style>
